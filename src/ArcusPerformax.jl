d404f94cc5140dfd6fac453f160d14e76b971881
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2022-05-13 16:55:26 +0200

    Source low-level code

--- src/ArcusPerformax.jl
+++ src/ArcusPerformax.jl
@@ -1,5 +1,13 @@
 module ArcusPerformax
 
-# Write your package code here.
+# Load the low level interface to the Arcus driver.
+let file = joinpath(@__DIR__, "..", "deps", "deps.jl")
+    if !isfile(file)
+        error("File \"$file\" does not exists.  You may may generate it by:\n",
+              "    using Pkg\n",
+              "    Pkg.build(\"$(@__MODULE__)\")")
+    end
+    include(file)
+end
 
 end
